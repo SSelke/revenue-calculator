e557ca5eaf5d9f64a5f15be9ad6bc6a441ac050f
Scott <pravaca22@gmail.com>
2018-09-19 12:23:10 -0700

    update safari

--- src/components/MainCalculator/Tabs/Expenses/ExpenseData/ExpenseData.js
+++ src/components/MainCalculator/Tabs/Expenses/ExpenseData/ExpenseData.js
@@ -5,12 +5,12 @@ import classes from './ExpenseData.css';
 const expenseData = (props) => {
 
     return (
-        <tr>
+        <tr className={classes.TableRow}>
             <td>
                 <Button bsStyle="danger" bsSize="xsmall" className={classes.Button} onClick={() => props.delete(props.index)}>Delete</Button>
             </td>
-            <td><input type="text" value={props.name} onChange={(event) => props.updateName(event.target.value, props.index)} /></td>
-            <td><input type="text" value={props.category} onChange={(event) => props.updateCategory(event.target.value, props.index)} /></td>
+            <td><input style={{ width: '100%' }} type="text" value={props.name} onChange={(event) => props.updateName(event.target.value, props.index)} /></td>
+            <td><input style={{ width: '100%' }} type="text" value={props.category} onChange={(event) => props.updateCategory(event.target.value, props.index)} /></td>
             <td><span>$<input type="number" style={{textAlign: 'left', width: '80px'}} value={props.amount} onChange={(event) => props.updateAmount(event.target.value, props.index)} /></span></td>
         </tr>
     );
--- src/components/MainCalculator/Tabs/Products/TableData/TableData.js
+++ src/components/MainCalculator/Tabs/Products/TableData/TableData.js
@@ -16,15 +16,15 @@ const tableData = (props) => {
                     onChange={(event) => props.updateName(event.target.value, props.index, props.type)} />
             </td>
             <td className={classes.Data}>
-                <span style={{ marginLeft: '58px' }}>$
-                        <input type="number" style={{ width: '100px', textAlign: 'left' }}
+                <span style={{ marginLeft: '18px' }}>$
+                        <input type="number" style={{ width: '80px', textAlign: 'left' }}
                         value={props.cost}
                         onChange={(event) => props.updateCost(event.target.value, props.index, props.type)} />
                 </span>
             </td>
             <td className={classes.Data}>
                 <span>
-                    <input type="number" style={{ width: '60px', textAlign: 'right' }}
+                    <input type="number" style={{ width: '40px', textAlign: 'right' }}
                         step="0.25"
                         value={props.time}
                         onChange={(event) => props.updateTime(event.target.value, props.index, props.type)} />
